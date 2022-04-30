0a597be24ebfef302b4e34c13675e6dd01f1efa2
Boto Kardava <102715964+Shwifty883@users.noreply.github.com>
2022-04-30 10:30:14 +0400

    Update AssignmentEight.java
    საბოლოო დააპდეითებული ვერსია

--- AssignmentEight.java
+++ AssignmentEight.java
@@ -2,40 +2,50 @@ import java.util.Stack;
 
 public class AssignmentEight {
     public static void main(String[] args) {
-        System.out.println(isPatternCorrect("((("));
-        System.out.println(isPatternCorrect("{}"));
-        System.out.println(isPatternCorrect("(()){}[]"));
-        System.out.println(isPatternCorrect("{({})}"));
+//        System.out.println(isPatternCorrect("[()}"));
+//        System.out.println(isPatternCorrect("[()]"));
+//        System.out.println(isPatternCorrect("(({}))[]"));
+//        System.out.println(isPatternCorrect("())"));
+//        System.out.println(isPatternCorrect("((("));
+//        System.out.println(isPatternCorrect("{}{}[]()"));
+//        System.out.println(isPatternCorrect("))"));
+//        System.out.println(isPatternCorrect("{)"));
+        System.out.println(isPatternCorrect("{}]"));
+        System.out.println(isPatternCorrect(""));
+
+        //calling the second task
+//        Queue myQueueObj = new Queue();
+//        myQueueObj.push(10);
+//        myQueueObj.peek();
+//        myQueueObj.pop();
+//        myQueueObj.size();
+
     }
 
     private static boolean isPatternCorrect(String pattern) {
-        char firstChar = '(';
-        char secondChar = '{';
-        char thirdChar = '[';
-        char reversedFirst = ')';
-        char reveredSecond = '}';
-        char reversedThird = ']';
-
         Stack<Character> myPattern = new Stack<>();
         char[] charsArray = pattern.toCharArray();
 
-        if (pattern.length() % 2 != 0) {
+        if (pattern.length() == 0) {
+            return true;
+        } else if (pattern.length() % 2 != 0) {
+            return false;
+        } else if (charsArray[0] == ')' || charsArray[0] == '}' || charsArray[0] == ']') {
             return false;
         }
 
         for (char i : charsArray) {
-            if (i == firstChar || i == secondChar || i == thirdChar) {
+            if (i == '(' || i == '{' || i == '[') {
                 myPattern.push(i);
-            } else if (i == reversedFirst || i == reveredSecond || i == reversedThird) {
+            } else if (i == ')' || i == '}' || i == ']') {
                 char poppedChar = myPattern.pop();
-                //აქ თვითონ სიმპლიფაიო თორემ მე ასე არ მეწერა :დდ
-                return poppedChar == firstChar && i == reversedFirst ||
-                        poppedChar == secondChar && i == reveredSecond ||
-                        poppedChar == thirdChar && i == reversedThird;
+                if (poppedChar == '(' && i != ')' ||
+                        poppedChar == '{' && i != '}' ||
+                        poppedChar == '[' && i != ']') {
+                    return false;
+                }
             }
         }
-
-        //აქაც სიმპლიფაიო..
         return myPattern.isEmpty();
     }
 }
